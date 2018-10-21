a1449b42f9a0feaba2d93f7a8772dfb17aebda96
Amanda Pereira <afp1103@gmail.com>
2018-10-21 17:53:25 +0000

    Commit inicial.

---
+++ app.js
@@ -0,0 +1,35 @@
+
+let express = require('express'),
+    app = express();
+
+let path = require('path');
+
+let fs = require('fs');
+
+let db = {
+	node: JSON.parse(fs.readFileSync(__dirname + "/data/json/node.json")),
+  NovoEdges: JSON.parse(fs.readFileSync(__dirname + "/data/json/NovoEdges.json")),
+  sumAllAlies: JSON.parse(fs.readFileSync(__dirname + "/data/json/sumAllAlies.json")),
+  sumAllEnemies: JSON.parse(fs.readFileSync(__dirname + "/data/json/sumAllEnemies.json")),
+  sumUniqueAlly: JSON.parse(fs.readFileSync(__dirname + "/data/json/sumUniqueAlly.json")),
+  sumUniqueEnemy: JSON.parse(fs.readFileSync(__dirname + "/data/json/sumUniqueEnemy.json"))
+};
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+let server = app.listen(process.env.PORT || 3000, function () {
+  console.log('Escutando em: http://localhost:3000');
+});
+
+app.get('/', function(request, response) {
+  response.render('index', db.dados);
+});
+
+/*app.get('/login', function(request, response) {
+  response.render('login');
+});*/
+
+module.exports = app;
